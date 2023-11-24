ba0034a9c6acc1943a0d22535af6ff1761befd9a
JazCh04 <jazchillagana@gmail.com>
2023-11-24 17:22:06 -0500

    Carga inicial del codigo de Software Deportivo

---
+++ #Software Deportivo.py
@@ -0,0 +1,7 @@
+
+#Bienvenida
+print ("Bienvenid@ a la aplicación FitEnergy")
+
+#Mostrar pantalla de ingreso aplicación
+#Librearia tomada de tkinter para interfaz
+from tkinter import Tk, Label, Button
