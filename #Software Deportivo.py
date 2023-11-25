3a17b42fa0c8d80f99580debedca28f50d90c11e
JazCh04 <jazchillagana@gmail.com>
2023-11-24 19:22:06 -0500

    LIBRERÍA KIVY

--- #Software Deportivo.py
+++ #Software Deportivo.py
@@ -2,6 +2,10 @@
 #Bienvenida
 print ("Bienvenid@ a la aplicación FitEnergy")
 
-#Mostrar pantalla de ingreso aplicación
-#Librearia tomada de tkinter para interfaz
-from tkinter import Tk, Label, Button
+#Extraer etiqueta de los paquetes UIX de Kivy
+from kivy.uix.label import Label
+
+#Crear aplicación principal
+class SimpleKivy:
+    def build(self):
+         return Label(text="Bienvenido")
