d18d299804dbd2a5745bd9ff79190bb608a0dca5
JazCh04 <jazchillagana@gmail.com>
2023-12-05 21:39:47 -0500

    INCLUIR PANTALLA

--- #Software Deportivo.py
+++ #Software Deportivo.py
@@ -1,7 +1,4 @@
 
-#Bienvenida
-print ("Bienvenid@ a la aplicación FitEnergy")
-
 #Importar marcos para app y widget de diseño
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -10,12 +7,27 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+from functools import partial
 
 #Importar interfaz para web de usuarios
 import webbrowser
 
 #Crear pantalla
+class SimpleScreen (BoxLayout):
+    def _init_(self,**kwargs):
+        super (SimpleScreen, self)._init_(**kwargs)
+        self.orientacion = 'vertical'
+        self.add_widget(Label(text = 'Bienvenid@ a la aplicación FitEnergy'))   
 
+#Mantener la pantalla abierta
+while True:
+    class SimpleApp (App):
+        def build(self):
+            return SimpleScreen ()
+    
+    if __name__ =='_main_':
+        SimpleApp().run()
 
 #Colocar boton visualizacion en el menu principal
 class MyButton (Button):
